3a3d55c205c3f5444ba6dd7bfa317a73608642e6
Francesco Muia <muia2987@gmail.com>
2023-10-25 18:24:06 +0200

    Added plot creation and function create_sliders.

--- app_dev.py
+++ app_dev.py
@@ -5,6 +5,7 @@
 from aux.imports import *
 from aux.data_files import detector_data
 from aux.aux_functions import load_data
+from aux.aux_functions import create_sliders
 
 # Load detector curves
 
@@ -18,3 +19,38 @@ print(LSD_weak)
 
 
 # Load signal curves
+
+
+
+
+
+
+# Set up the figure
+
+hmin = 10.**-35.
+hmax = 10.**-10.
+xrange=(10**-18, 10**20)
+fig = figure(background_fill_color='white',
+             border_fill_color='black',
+             border_fill_alpha=0.0,
+             height=600,
+             width=int(1.61803398875*600),
+             x_axis_label='frequency (Hz)',
+             x_axis_type='log',
+             x_axis_location='below',
+             x_range=xrange,
+             y_axis_label='h',
+             y_axis_type='log',
+             y_axis_location='left',
+             y_range=(hmin, hmax),
+             title='Gravitational waves plotter',
+             title_location='above',
+             toolbar_location='below',
+             tools='save')
+fig.output_backend = "svg"
+
+# Set up the sliders
+
+slider_x, slider_y, slider_width, slider_height = create_sliders(fig)
+
+print(slider_x)
--- aux/aux_functions.py
+++ aux/aux_functions.py
@@ -1,5 +1,6 @@
 # aux_functions.py
 import numpy as np
+from bokeh.models import RangeSlider, CustomJSTickFormatter, CustomJS, Slider
 
 class Data:
     def __init__(self, x_coord, y_coord, label, category=None):
@@ -15,4 +16,40 @@ def load_data(file_path, label):
     x_coord, y_coord = data[:, 0], data[:, 1]
     return Data(x_coord, y_coord, label)
 
+def create_sliders(fig):
+    range_slider_x = RangeSlider(
+        title=" Adjust frequency range",
+        start=-18.,
+        end=20.,
+        step=1,
+        value=(np.log10(float(fig.x_range.start)), np.log10(float(fig.x_range.end))),
+        format=CustomJSTickFormatter(code="return ((Math.pow(10,tick)).toExponential(0))")
+    )
+    range_slider_x.js_on_change('value',
+        CustomJS(args=dict(other=fig.x_range),
+                 code="other.start = 10**(this.value[0]);other.end = 10**(this.value[1]);"
+        )
+    )
+    range_slider_y = RangeSlider(
+        title=" Adjust strain range",
+        start=-35.,
+        end=-10.,
+        step=1,
+        value=(np.log10(float(fig.y_range.start)), np.log10(float(fig.y_range.end))),
+        format=CustomJSTickFormatter(code="return ((Math.pow(10.,tick)).toExponential(0))")
+    )
+    range_slider_y.js_on_change('value',
+        CustomJS(args=dict(other=fig.y_range),
+                 code="other.start = 10**(this.value[0]);other.end = 10**(this.value[1]);"
+        )
+    )
+    slider_width = Slider(title="Adjust plot width", start=320, end=1920, step=10, value=int(1.61803398875*600))
+    callback_width = CustomJS(args=dict(plot=fig, slider=slider_width), code="plot.width = slider.value;")
+    slider_width.js_on_change('value', callback_width)
+
+    slider_height = Slider(title="Adjust plot height", start=240, end=1080, step=10, value=600)
+    callback_height = CustomJS(args=dict(plot=fig, slider=slider_height), code="plot.height = slider.value;")
+    slider_height.js_on_change('value', callback_height)
+
+    return range_slider_x, range_slider_y, slider_width, slider_height  # return the sliders if needed
 
